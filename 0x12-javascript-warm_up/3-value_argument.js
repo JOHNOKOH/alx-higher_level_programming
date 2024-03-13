c1abbdc2fd36019661d878e2e3447739ba4fe7b5
JOHNOKOH <johnokoh735@gmail.com>
2024-03-13 17:35:02 +0000

    value arguments

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
-  console.log("No argument");
-} else {
-  console.log(args[0]);
-}
+console.log(process.argv[2] ? process.argv[2] : 'No argument');
